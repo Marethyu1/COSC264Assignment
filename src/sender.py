fd4047f1383bd7fbb77656bc7543579b9287bbb6
Stefan <sha162@uclive.ac.nz>
2016-08-31 19:41:58 +1200

    Sender and reciever working

--- src/sender.py
+++ src/sender.py
@@ -8,10 +8,14 @@ python sender.py portNum PortNum PortNum fileName
 import sys
 import socket
 import os.path #This is being used to check if file exists
+import struct
+import binascii
 
 
 MAX_BYTES = 512
 MAGICNO = hex(0x497E)
+PTYPE_DATA = 0
+PTYPE_ACK = 1
 
 def exit():
     #exits program
@@ -67,8 +71,8 @@ def read_file_data(file_obj):
 
 
 class packet:
-    def __init__(self, type, seqno, dataLen, data):
-        self.magicno = MAGICNO
+    def __init__(self, seqno, dataLen, data, type=PTYPE_DATA, magicno=MAGICNO):
+        self.magicno = magicno
         self.type = type
         self.seqno = seqno
         self.dataLen = dataLen
@@ -78,15 +82,65 @@ class packet:
         """returns true if Magicno == """
         return self.magicno == MAGICNO
 
+    def __str__(self):
+        current_type = "dataPacket"
+        if self.type == PTYPE_ACK:
+            current_type = "acknowledgementPacket"
+        out_string = "\ntype is {0}\nSeqno is {1}\nDataLen is {2}\nData is {3}\nmagicNo is {4}\n".format(current_type, self.seqno, self.dataLen, self.data, self.isMagicno())
+        # a = ("Type is {0}\n".format(self.type))
+        # b = ("Seqno is {0}\n".format(self.seqno))
+        # c = ("DataLen is {0}\n".format(self.dataLen))
+        # d = ("Data is {0}\n".format(self.data))
+        # e = ("magicNo is {0}\n".format(self.magicno))
+        return out_string
+
+def pack_packet(current_packet):
+
+    magicno = current_packet.magicno
+    type = current_packet.type
+    seqno = current_packet.seqno
+    dataLen = current_packet.dataLen
+    data = current_packet.data
+
+
+    to_pack = (int(magicno, 0), type, seqno, dataLen, data)
+
+    pack_format = 'I I I I {0}s'.format(dataLen)
+    my_struct = struct.Struct(pack_format)
+    packed_packet = my_struct.pack(*to_pack)
+
+    return packed_packet
+
+def unpack_packet(packed_packet):
+
+    pack_format = 'I I I I'
+    my_struct = struct.Struct(pack_format)
+
+    unpacked_packet = my_struct.unpack(packed_packet)
+    # magicno = unpacked_packet[0]
+    # type = unpacked_packet[1]
+    # seqno = unpacked_packet[2]
+    dataLen = unpacked_packet[3]
+    # data = unpacked_packet[4]
+
+    print(dataLen)
+    print()
+    print()
+
+
+
+
+
+
 def main():
     #this is the main function which does the business
     SIN, SOUT, CSIN, FILENAME = get_params()
-    print(SIN, SOUT, CSIN, FILENAME)
+    #print(SIN, SOUT, CSIN, FILENAME)
 
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     sock.bind(('127.0.0.1', SIN))
     sock.connect(('127.0.0.1', SOUT))  # So we don't have to specify where we send to
-   # sock.send(b'Hello Liz and Stefan!')  # Remember, bytes not strings
+    # sock.send(b'Hello Liz and Stefan!')  # Remember, bytes not strings
     next = 0
     exitFlag = False
 
@@ -94,15 +148,38 @@ def main():
 
     file_object = open(FILENAME, "rb")
 
-    file_string = read_file_data(file_object)
-    print(file_string)
+    #current_string = read_file_data(file_object)
+    """print(file_string)
     print()
 
     while len(file_string) != 0:
         file_string = read_file_data(file_object)
         print(file_string)
         print()
+    """
+    count  = 0
+    while not exitFlag:
+        current_string = read_file_data(file_object)
+        if len(current_string) == 0:
+            current_packet = packet(next, len(current_string), current_string)
+            #print(current_packet)
 
+        elif len(current_string) > 0:
+            current_packet = packet(next, len(current_string), current_string)
+            #print(current_packet)
+
+
+        encoded_packet = pack_packet(current_packet)
+        unpacked_packet = unpack_packet(encoded_packet)
+
+
+        count += 1
+
+        if count == 10:
+            exitFlag = True
+
+
+    file_object.close()
     """Attempt to read up to 512 byes from file to buffer. n = number of bytes actually read
         If n==0 prepare packet:
             *magicno = 0x497E
